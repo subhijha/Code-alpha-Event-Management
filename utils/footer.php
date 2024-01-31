f6279543824f3f3ca72764817f6829c40fce8049
Bhagwatiasthanrahua <manoharvats07@gmail.com>
2024-01-31 13:13:03 +0530

    comiit

---
+++ utils/footer.php
@@ -0,0 +1,38 @@
+<hr class = "footerline"><!--css modified horizontal line-->
+<footer>
+    <div class = "container">
+        <div class = "row">
+            <section>
+                <div class = "footerContent col-md-4"><!--left content-->
+                    <p class = "footerContent1">
+                        <strong>E</strong><span class = "small footerSubtext">vent</span>
+                        <strong>M</strong><span class = "small footerSubtext">anagement</span>
+                        <strong>S</strong><span class = "small footerSubtext">ystem</span>
+                        <strong>P</strong><span class = "small footerSubtext">roject</span>
+                    </p>
+
+                    <p class = "footerSubtext2">
+                        Design &copy; Developed by 
+                        &copy; MANOHAR KUMAR JHA
+                    </p>
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--middle content-->
+                    UI/UX  Designer &copy; Full stack Developer<br>
+                    Creative Computing 2022-2025.<br>
+                    Bhopal, 462021
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--right content-->
+                    Follow Us:<br>
+                        <img src = "images/facebook.png">
+                        <img src = "images/twitter.png">
+                        <img src = "images/googleplus.png">
+                        <img src = "images/youtube.png">
+                </div>
+            </section>
+        </div>
+    </div>
+</footer>
